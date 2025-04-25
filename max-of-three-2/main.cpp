fe64112ddde457d6def5b2371195fab2bfee09f2
Moaz-Hassanein <moaza0902@gmail.com>
2025-04-25 11:56:00 +0300

    Update main.cpp

--- max-of-three-2/main.cpp
+++ max-of-three-2/main.cpp
@@ -5,20 +5,30 @@ typedef long long ll;
 
 
 int maxProductOfThree(int arr[],int size) {
-    int max1,max2,max3,index1,index2;
+    int max1,max2,max3,min1,min2,index1,index2,indexm1,indexm2;
     max1=arr[0];
+    min1=arr[0];
     for(int i=1;i<size;i++) {
         if(arr[i]>max1) {
             max1=arr[i];
             index1=i;
         }
+        if(arr[i]<min1) {
+            min1=arr[i];
+            indexm1=i;
+        }
     }
 
     max2=arr[0];
+    min2=arr[0];
     for(int i=1;i<size;i++) {
         if(arr[i]>max2 && i!=index1) {
             max2=arr[i];
             index2=i;
+        }
+        if(arr[i]<min2 && i!=indexm1) {
+            min2=arr[i];
+            indexm2=i;
         }
     }
 
@@ -29,14 +39,6 @@ int maxProductOfThree(int arr[],int size) {
         }
     }
 
-    int result = max1*max2*max3;
+    int result = max(max1*max2*max3,max1*min1*min2);
     return result;
-}
-
-int main() {
-    int arr[]={16,14,10,8,7,9,3,2,4,1,200,10,20,30};
-    int ans=maxProductOfThree(arr,sizeof(arr)/sizeof(arr[0]));
-    for (int i=0;i<(sizeof(arr)/sizeof(arr[0]));i++)
-        cout<< arr[i]<<"  ";
-    cout<<"\n"<< ans;
 }
