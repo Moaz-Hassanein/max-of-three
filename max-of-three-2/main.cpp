2ab76f0ef831087efeb096a278e19c9e99003051
Moaz-Hassanein <moaza0902@gmail.com>
2025-04-25 12:50:08 +0300

    Update main.cpp

--- max-of-three-2/main.cpp
+++ max-of-three-2/main.cpp
@@ -1,58 +1,25 @@
 int maxProductOfThree(int arr[],int size) {
-    int max1,max2,max3,min1,min2,index1,index2,indexm1;
-    max1=arr[0];
-    index1=0;
-    min1=arr[0];
-    indexm1=0;
-    for(int i=0;i<size;i++) {
-        if(arr[i]>max1) {
-            max1=arr[i];
-            index1=i;
+    if (size < 3)
+        return 0;
+    int max1 = INT_MIN, max2 = INT_MIN, max3 = INT_MIN;
+    int min1 = INT_MAX, min2 = INT_MAX;
+    for (int i = 0; i < size; i++) {
+        if (arr[i] > max1) {
+            max3 = max2;
+            max2 = max1;
+            max1 = arr[i];
+        } else if (arr[i] > max2) {
+            max3 = max2;
+            max2 = arr[i];
+        } else if (arr[i] > max3) {
+            max3 = arr[i];
         }
-        if(arr[i]<min1) {
-            min1=arr[i];
-            indexm1=i;
-        }
-    }
-
-    if(index1 !=0) {
-        max2=arr[0];
-        index2=0;
-    }
-    else {
-        max2=arr[1];
-        index2=1;
-    }
-    if(indexm1 !=0)
-        min2=arr[0];
-    else
-        min2=arr[1];
-    for(int i=0;i<size;i++) {
-        if(arr[i]>max2 && i!=index1) {
-            max2=arr[i];
-            index2=i;
-        }
-        if(arr[i]<min2 && i!=indexm1) {
-            min2=arr[i];
+        if (arr[i] < min1) {
+            min2 = min1;
+            min1 = arr[i];
+        } else if (arr[i] < min2) {
+            min2 = arr[i];
         }
     }
-    if(index1 !=0 && index2 !=0 )
-        max3=arr[0];
-    else {
-        max3=max1+1;
-        int i=1;
-        while(max3==max1+1) {
-            if(index1 !=i && index2 !=i)
-                max3=arr[i];
-            i++;
-        }
-    }
-    for(int i=0;i<size;i++) {
-        if(arr[i]>max3 && i!=index1 && i!= index2) {
-            max3=arr[i];
-        }
-    }
-
-    int result = max(max1*max2*max3,max1*min1*min2);
-    return result;
+    return max(max1 * max2 * max3, max1 * min1 * min2);
 }
