07d9aa354e2421c8b302d7b09f53c9065960fdb4
Moaz-Hassanein <moaza0902@gmail.com>
2025-04-25 12:28:03 +0300

    Update main.cpp

--- max-of-three-2/main.cpp
+++ max-of-three-2/main.cpp
@@ -1,14 +1,10 @@
-#include <bits/stdc++.h>
-
-using namespace std;
-typedef long long ll;
-
-
 int maxProductOfThree(int arr[],int size) {
-    int max1,max2,max3,min1,min2,index1,index2,indexm1,indexm2;
+    int max1,max2,max3,min1,min2,index1,index2,indexm1;
     max1=arr[0];
+    index1=0;
     min1=arr[0];
-    for(int i=1;i<size;i++) {
+    indexm1=0;
+    for(int i=0;i<size;i++) {
         if(arr[i]>max1) {
             max1=arr[i];
             index1=i;
@@ -19,21 +15,39 @@ int maxProductOfThree(int arr[],int size) {
         }
     }
 
-    max2=arr[0];
-    min2=arr[0];
-    for(int i=1;i<size;i++) {
+    if(index1 !=0) {
+        max2=arr[0];
+        index2=0;
+    }
+    else {
+        max2=arr[1];
+        index2=1;
+    }
+    if(indexm1 !=0)
+        min2=arr[0];
+    else
+        min2=arr[1];
+    for(int i=0;i<size;i++) {
         if(arr[i]>max2 && i!=index1) {
             max2=arr[i];
             index2=i;
         }
         if(arr[i]<min2 && i!=indexm1) {
             min2=arr[i];
-            indexm2=i;
         }
     }
-
-    max3=arr[0];
-    for(int i=1;i<size;i++) {
+    if(index1 !=0 && index2 !=0 )
+        max3=arr[0];
+    else {
+        max3=max1+1;
+        int i=1;
+        while(max3==max1+1) {
+            if(index1 !=i && index2 !=i)
+                max3=arr[i];
+            i++;
+        }
+    }
+    for(int i=0;i<size;i++) {
         if(arr[i]>max3 && i!=index1 && i!= index2) {
             max3=arr[i];
         }
